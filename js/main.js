e30ee206b6f0321d5342b73f9e56d3d216b18c82
Godwin Kamau <contactgodwinkamau@gmail.com>
2025-09-11 22:00:20 -0400

    added table and adding value

---
+++ js/main.js
@@ -0,0 +1,32 @@
+document.querySelector('#new-task').addEventListener('click',newRow)
+
+// Obtain relevant interface elements
+const bodySection = document.querySelectorAll("tbody")[0];
+
+
+function newRow() {
+    const row = bodySection.rows.item(bodySection.rows.length - 1); // Select the first row of the body section
+    // const cells = row.cells; // The collection is live, therefore always up-to-date
+
+    console.log('started')
+    let newCell = row.insertCell(0)
+    newCell.innerHTML = '<input type="checkbox">'
+
+    let newCell2 = row.insertCell(1)
+    newCell2.textContent = document.getElementById('new-todo').value
+    newCell2.className = 'task'
+
+    let newCell3 = row.insertCell(2)
+    newCell3.textContent = document.getElementById('new-due').value
+    newCell3.className = 'dueDate'
+
+    bodySection.insertRow()
+    // let table = document.getElementById('to-do')
+    // let newRow = table.insertRow()
+
+    // let task = newRow.insertCell()
+    // let newText = document.getElementById('new-todo').value
+    // let insertText = document.createTextNode(newText)
+    // task.appendChild(insertText)
+
+}
